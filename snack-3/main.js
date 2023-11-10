1d586aa26a047e0f9e4eba76789eef650559a9ec
danielecarpentiero <carpentierodaniele@gmail.com>
2023-11-10 12:21:00 +0100

    fix: array

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,43 +1,18 @@
-const numero1String = prompt("inserisci il primo numero");
-const numero1 = Number(numero1String);
+const numeri = [];
+let sommaNumeri = 0;
 
-const numero2String = prompt("inserisci il secondo numero");
-const numero2 = Number(numero2String);
+for (let i = 1; i <= 10; i++) {
+  const numeroString = prompt(`Inserisci il ${i}° numero`);
+  const numero = Number(numeroString);
 
-const numero3String = prompt("inserisci il terzo numero");
-const numero3 = Number(numero3String);
+  if (!isNaN(numero)) {
+    numeri.push(numero);
+    sommaNumeri += numero;
+  } else {
+    console.log("Input non valido. Inserisci un numero valido.");
+    i--; // Decrementa l'indice per ripetere la stessa iterazione
+  }
+}
 
-const numero4String = prompt("inserisci il quarto numero");
-const numero4 = Number(numero4String);
-
-const numero5String = prompt("inserisci il quinto numero");
-const numero5 = Number(numero5String);
-
-const numero6String = prompt("inserisci il sesto numero");
-const numero6 = Number(numero6String);
-
-const numero7String = prompt("inserisci il settimo numero");
-const numero7 = Number(numero7String);
-
-const numero8String = prompt("inserisci il ottavo numero");
-const numero8 = Number(numero8String);
-
-const numero9String = prompt("inserisci il nono numero");
-const numero9 = Number(numero9String);
-
-const numero10String = prompt("inserisci il decimo numero");
-const numero10 = Number(numero10String);
-
-const sommaNumeri =
-  numero1 +
-  numero2 +
-  numero3 +
-  numero4 +
-  numero5 +
-  numero6 +
-  numero7 +
-  numero8 +
-  numero9 +
-  numero10;
-
-console.log(`"La somma dei numeri da te inseriti è ${sommaNumeri}"`);
+console.log(`La somma dei numeri da te inseriti è ${sommaNumeri}`);
+console.log("Array di numeri inseriti:", numeri);
