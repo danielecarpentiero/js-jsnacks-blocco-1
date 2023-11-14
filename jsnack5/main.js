dc8ee5e22393180588e16429c5f2db9df9b34f79
danielecarpentiero <carpentierodaniele@gmail.com>
2023-11-14 12:10:57 +0100

    fix: calcolo corretto

--- jsnack5/main.js
+++ jsnack5/main.js
@@ -3,8 +3,8 @@ let arrayN = [];
 
 for (let i = 0; i < n; i++) {
   let arrayNTwo = [];
-  for (let j = 0; j < n; j++) {
-    arrayNTwo.push(Math.floor(Math.random() * 100));
+  for (let j = 0; j < 10; j++) {
+    arrayNTwo.push(Math.floor(Math.random() * 100) + 1);
   }
   arrayN.push(arrayNTwo);
 }
